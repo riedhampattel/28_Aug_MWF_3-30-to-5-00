e3e0f874650b5d1d5838c02c5aa9ca5268e48891
riedhampattel <riedhampattel.tops@gmail.com>
2024-09-06 16:32:43 +0530

    Add files via upload

---
+++ C Programming/Basic Syntax/4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char ch1 = 'TOPS';
+	char ch2,ch3,ch4;
+	
+	printf("\nEnter the value of ch2 = ");
+	scanf(" %c",&ch2);
+	
+	printf("\nValue of ch1 = %c",ch1);
+	printf("\nValue of ch2 = %c",ch2);
+
+	return 0;
+}
