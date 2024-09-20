e350846665d478b9c0b7ad31e94e8528994547f2
riedhampattel <riedhampattel.tops@gmail.com>
2024-09-20 16:26:21 +0530

    updated

--- C Programming/Loops/p8.c
+++ C Programming/Loops/p8.c
@@ -12,9 +12,9 @@ int main()
 {
 	int row,i,j,p,q;
 	printf("\nEnter the row count = ");
-	scanf("%d",&row);//3
+	scanf("%d",&row);
 	
-	for(i=1;i(4)<=row(3);i++)
+	for(i=1;i<=row;i++)
 	{
 		if(i%2==0)
 		{
@@ -26,7 +26,7 @@ int main()
 			p = 1;
 			q = 0;
 		}
-		for(j=1;j(4)<=i(3);j++)
+		for(j=1;j<=i;j++)
 		{
 			if(j%2==0)
 			{
