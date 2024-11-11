8d352856631d7f6dfb1bd8559fd9d8c61b89d7c8
riedhampattel <riedhampattel.tops@gmail.com>
2024-11-11 16:29:09 +0530

    updated

--- DBMS/3.sql
+++ DBMS/3.sql
@@ -55,3 +55,45 @@ select * from employees where first_name like '%e_';
 select * from employees where hire_date like '%-J_n-%';
 
 select * from employees where hire_date like '%-Ju_-%';
+
+select * from employees where department_id = 90 or department_id = 60 or department_id = 50;
+
+select * from employees where department_id in (90,60,50);
+
+select * from employees where department_id not in (90,60,50);
+
+select * from employees where commission_pct is not null;
+
+select * from employees where commission_pct is null;
+
+select distinct department_id from employees;
+
+select * from employees;
+
+select min(salary) from employees;
+
+select max(salary) from employees;
+
+select sum(salary) from employees;
+
+select avg(salary) from employees;
+
+select count(commission_pct) from employees;
+
+select department_id,min(salary) from employees group by department_id;
+
+select department_id,min(salary) from employees group by department_id having min(salary)>10000;
+
+select department_id,max(salary) from employees group by department_id having min(salary)>10000;
+
+select department_id,max(salary) from employees group by department_id having max(salary)>10000;
+
+select * from employees order by salary desc limit 5;
+
+select * from employees order by salary limit 5;
+
+select * from employees;
+
+select * from employees limit 10 offset 5;
+
+select * from employees order by salary limit 10 offset 6;
